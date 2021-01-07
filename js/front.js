874956595a9949e61bfbdbe9197535a146e8d69a
eybaek <eybaek22@gmail.com>
2021-01-07 12:29:03 +0900

    update map

--- js/front.js
+++ js/front.js
@@ -253,7 +253,7 @@ $(function () {
 function map() {
 
     var mapId = 'map',
-        mapCenter = [53.14, 8.22],
+        mapCenter = [37.22, 127.06],
         mapMarker = true;
 
     if ($('#' + mapId).length > 0) {
